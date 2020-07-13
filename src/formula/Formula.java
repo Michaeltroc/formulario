09378a26a14db02a4c42ac94edeb0c79f310978b
MichaelTc <mitrocollazos@unicauca.edu.co>
2020-07-13 15:18:32 -0500

    Formulario desde netbeans

---
+++ src/formula/Formula.java
@@ -0,0 +1,16 @@
+package formula;
+
+import formula.acceso.acceso;
+
+/**
+ * @author MichaelTc
+ */
+public class Formula {
+
+    public static void main(String[] args) {
+        
+        acceso acces = new  acceso();
+        acces.setVisible(true);
+    }
+    
+}
---
+++ src/formula/Persona.java
@@ -0,0 +1,54 @@
+package formula;
+
+/**
+ * @author MichaelTc
+ */
+public class Persona {
+    
+    String nombre,apellido,edad,numero;
+
+    public Persona(String nombre, String apellido, String edad, String numero) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
+    
+    
+    
+    
+    
+}
